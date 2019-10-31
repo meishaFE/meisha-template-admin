7c1a2a20a4a2a23180657a452f45701bdf3e0d1c
Lhongyu <463373718@qq.com>
2019-10-31 13:44:38 +0800

    pull

---
+++ generator/base/src/store/mutation.js
@@ -0,0 +1,15 @@
+export default {
+  SETAUTH (state, val) {
+    state.auth = val;
+  },
+  SET_PUBLIC_1 (state, val) {
+    state.public1 = val;
+  },
+  SET_PUBLIC_2 (state, val) {
+    state.public2 = val;
+  },
+
+  SET_PUBLIC_3 (state, val) {
+    state.public3 = val;
+  }
+}
